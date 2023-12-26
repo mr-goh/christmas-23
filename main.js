4779c397a8f30fe20a4f66124f3eace2e2d15caf
mr-goh <72425327+mr-goh@users.noreply.github.com>
2023-12-26 16:53:47 -0600

    Update main.js

--- main.js
+++ main.js
@@ -1,4 +1,5 @@
-import * as THREE from 'three';
+// import * as THREE from 'three';
+import * as THREE from './node_modules/three'
 import { GLTFLoader } from 'three/addons/loaders/GLTFLoader.js';
 import { OrbitControls } from 'three/addons/controls/OrbitControls.js';
 
@@ -16,7 +17,7 @@ document.getElementById('model-container').appendChild(renderer.domElement);
 const loader = new GLTFLoader();
 let model;
 
-loader.load( './cactus.glb', function ( gltf ) {
+loader.load( 'cactus.glb', function ( gltf ) {
     
     model = gltf.scene;
     model.scale.set(1, 1, 1);
