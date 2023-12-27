2c3d1ce66ffd368dea80ec218f40ab1f2cd90964
mr-goh <72425327+mr-goh@users.noreply.github.com>
2023-12-26 19:52:36 -0600

    fix issue

--- main.js
+++ main.js
@@ -1,4 +1,4 @@
-import * as THREE from 'https://cdn.jsdelivr.net/npm/three@0.148.0/build/three.min.js';
+import * as THREE from './node_modules/three/build/three.module.js';
 import { GLTFLoader } from 'https://cdn.jsdelivr.net/npm/three/examples/jsm/loaders/GLTFLoader.js';
 import { OrbitControls } from 'https://cdn.jsdelivr.net/npm/three/examples/jsm/controls/OrbitControls.js';
 
