061e78d6b10a0be9b9feb8d387452f0857d1c4fd
mr-goh <72425327+mr-goh@users.noreply.github.com>
2023-12-26 18:53:37 -0600

    Update main.js

--- main.js
+++ main.js
@@ -1,5 +1,5 @@
 // import * as THREE from 'three';
-import * as THREE from './node_modules/three/build/three.module.js';
+import * as THREE from 'https://github.com/mr-goh/christmas-23/node_modules/three/build/three.module.js';
 import { GLTFLoader } from './node_modules/three/addons/loaders/GLTFLoader.js';
 import { OrbitControls } from './node_modules/three/addons/controls/OrbitControls.js';
 
